9a71b15095fe6a1ce0154e968696d9259ac26c2c
Hector Alpizar <hector.alpizar.c@gmail.com>
2025-03-29 15:27:18 -0700

    Arguments added to main method.

--- Game/game.cpp
+++ Game/game.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello world");
+int main(int argc, char * argv[]) {
+
+	char * greeting = "world";
+
+	if (argv[1] != NULL) {
+		greeting = argv[1];
+	}
+
+	printf("Hello %s.\n", greeting);
 	return 0;
 }
