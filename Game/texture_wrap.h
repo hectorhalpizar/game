cc2412d5e59a648d8c7a71c83fd2f308659b172b
Hector H Alpizar <hector.alpizar.c@gmail.com>
2025-04-02 20:50:41 -0700

    Redundant files removed.

--- Game/texture_wrap.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GAME_TEXTURE_WRAP_H__
-#define __GAME_TEXTURE_WRAP_H__
-
-#define START_GAME_TEXTURE_WRAP = "GameTextureWrap"
-
-int StartTextureWrap(int argc, char *argv[]);
-
-#endif // __GAME_TEXTURE_WRAP_H__
