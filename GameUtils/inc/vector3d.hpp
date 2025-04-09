3a122ae5f4c148db47419bcdb2163c3e779fc8df
Hector H Alpizar <hector.alpizar.c@gmail.com>
2025-04-09 07:54:09 -0700

    GameUtils - inlne methods fixed.

--- GameUtils/inc/vector3d.hpp
+++ GameUtils/inc/vector3d.hpp
@@ -1,6 +1,8 @@
 #ifndef __GAME_UTILS_VECTOR3D_HPP__
 #define __GAME_UTILS_VECTOR3D_HPP__
 
+#include <math.h>
+
 struct Vector3D
 {
 	float		x, y, z;
@@ -20,19 +22,42 @@ struct Vector3D
 	Vector3D& operator +=(const Vector3D& v);
 
 	Vector3D& operator -=(const Vector3D& v);
+
+private:
+	void init(float x, float y, float z);
 };
 
-inline Vector3D operator /(const Vector3D& v, float s);
+Vector3D operator *(const Vector3D& v, float s);
 
-inline Vector3D operator -(const Vector3D& v);
+inline Vector3D operator /(const Vector3D& v, float s)
+{
+	s = 1.0F / s;
+	return (Vector3D(v.x * s, v.y * s, v.z * s));
+};
 
-inline float Magnitude(const Vector3D& v);
+inline Vector3D operator *(const Vector3D& v, float s)
+{
+	return (Vector3D(v.x * s, v.y * s, v.z * s));
+};
 
-inline Vector3D Normalize(const Vector3D& v);
+inline float Magnitude(const Vector3D& v)
+{
+	return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
+}
 
-inline Vector3D operator +(const Vector3D& a, const Vector3D& b);
+inline Vector3D Normalize(const Vector3D& v)
+{
+	return (v / Magnitude(v));
+}
 
-inline Vector3D operator -(const Vector3D& a, const Vector3D& b);
+inline Vector3D operator +(const Vector3D& a, const Vector3D& b)
+{
+	return (Vector3D(a.x + b.x, a.y + b.y, a.z + b.z));
+}
 
+inline Vector3D operator -(const Vector3D& a, const Vector3D& b)
+{
+	return (Vector3D(a.x - b.x, a.y - b.y, a.z - b.z));
+}
 
 #endif // __GAME_UTILS_VECTOR3D_HPP__
--- GameUtils/src/vector3d.cpp
+++ GameUtils/src/vector3d.cpp
@@ -1,17 +1,24 @@
 #include "vector3d.hpp"
 
-#include <math.h>
+#include <stdio.h>
 
-Vector3D::Vector3D() {
+Vector3D::Vector3D(){
 	// Default values
+	init(0.f, 0.f, 0.f);
 };
 
 Vector3D::Vector3D(float x, float y, float z)
+{
+	init(x, y, z);
+}
+
+void Vector3D::init(float x, float y, float z)
 {
 	this->x = x;
 	this->y = y;
 	this->z = z;
 }
+
 
 float& Vector3D::operator [](int i)
 {
@@ -54,40 +61,4 @@ Vector3D& Vector3D::operator -=(const Vector3D& v)
 	y -= v.y;
 	z -= v.z;
 	return (*this);
-}
-
-inline Vector3D operator *(const Vector3D& v, float s)
-{
-	return (Vector3D(v.x * s, v.y * s, v.z * s));
-}
-
-inline Vector3D operator /(const Vector3D& v, float s)
-{
-	s = 1.0F / s;
-	return (Vector3D(v.x * s, v.y * s, v.z * s));
-}
-
-inline Vector3D operator -(const Vector3D& v)
-{
-	return (Vector3D(-v.x, -v.y, -v.z));
-}
-
-inline float Magnitude(const Vector3D& v)
-{
-	return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
-}
-
-inline Vector3D Normalize(const Vector3D& v)
-{
-	return (v / Magnitude(v));
-}
-
-inline Vector3D operator +(const Vector3D& a, const Vector3D& b)
-{
-	return (Vector3D(a.x + b.x, a.y + b.y, a.z + b.z));
-}
-
-inline Vector3D operator -(const Vector3D& a, const Vector3D& b)
-{
-	return (Vector3D(a.x - b.x, a.y - b.y, a.z - b.z));
 }
