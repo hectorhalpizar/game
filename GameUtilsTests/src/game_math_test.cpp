b2becfa3918723ff4d2420da74ef78e4ef994cc8
Hector H Alpizar <hector.alpizar.c@gmail.com>
2025-04-18 08:23:45 -0700

    GameUtilsTests - Unit Test files added

---
+++ GameUtilsTests/src/game_math_test.cpp
@@ -0,0 +1,74 @@
+#include "stdafx.h"
+
+#include <cmath>
+
+#include "TestTools/AssertExtention.hpp"
+#include "game_math.hpp"
+
+using namespace System;
+using namespace System::Text;
+using namespace System::Collections::Generic;
+using namespace	Microsoft::VisualStudio::TestTools::UnitTesting;
+using namespace GameUtills::UnitTesting;
+using namespace gameutils::math;
+
+namespace GameUtilsTests
+{
+	[TestClass]
+	public ref class GameMathTest
+	{
+	private:
+		TestContext^ testContextInstance;
+
+	public: 
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		property Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ TestContext
+		{
+			Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ get()
+			{
+				return testContextInstance;
+			}
+			System::Void set(Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ value)
+			{
+				testContextInstance = value;
+			}
+		};
+
+		#pragma region Additional test attributes
+		//
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//static void MyClassInitialize(TestContext^ testContext) {};
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//static void MyClassCleanup() {};
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//void MyTestInitialize() {};
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//void MyTestCleanup() {};
+		//
+		#pragma endregion 
+
+		[TestMethod]
+		void test_Game_Math_Default_constructor()
+		{
+			// Arrange & Act
+			const Point3D testInstance;
+			
+			// Assert
+			Assert::AreEqual(0.f, testInstance.x);
+			Assert::AreEqual(0.f, testInstance.y);
+			Assert::AreEqual(0.f, testInstance.z);
+		};
+	};	
+}
